5025efbf89252f340297599e7e5b53eef4ee34c8
Urszula Wielgosz <urszula@wielgosz.info>
2024-03-30 14:44:15 +0000

    Auto generate styles loader

--- includes/class-assets.php
+++ includes/class-assets.php
@@ -20,11 +20,6 @@ class Assets extends Utils\Singleton {
 		'viewScript',
 		'viewScriptModule',
 	);
-	private array $style_fields = array(
-		'editorStyle',
-		'style',
-		'viewStyle',
-	);
 
 	protected function __construct() {
 		$this->theme_slug = get_template();
@@ -35,7 +30,6 @@ class Assets extends Utils\Singleton {
 		add_action( 'enqueue_block_assets', array( $this, 'enqueue_editor_assets' ) );
 
 		add_filter( 'block_type_metadata', array( $this, 'update_block_type_metadata' ) );
-		// add_filter( 'block_type_metadata_settings', array( $this, 'update_block_type_metadata_settings' ), 10, 2 );
 	}
 
 	public function enqueue_frontend_assets(): void {
@@ -63,49 +57,68 @@ class Assets extends Utils\Singleton {
 		);
 	}
 
-	public function update_block_type_metadata_settings( array $settings, array $metadata ): array {
-		// Only for theme blocks
-		if ( str_starts_with( $metadata['name'], $this->theme_slug ) ) {
-			var_dump($settings);
-			var_dump($metadata);
+	/**
+	 * Automatically generates stylesheet import script if needed.
+	 * It will return metadata field value or false if no auto-generation is needed.
+	 *
+	 * @param array $metadata
+	 * @param string $style_key
+	 * @param string $script_key
+	 *
+	 * @return bool|string
+	 */
+	private function maybe_generate_style_import( $metadata, $style_key, $script_key ): bool|string {
+		if ( $metadata[ $style_key ] && is_null( $metadata[ $script_key ] ) ) {
+			trigger_error( sprintf( 'Block %s has %s but no corresponding script, auto-generating', $metadata['name'], $style_key ), E_USER_WARNING );
+
+			$block_path = dirname( $metadata['file'] );
+			$auto_import_file = $block_path . '/' . pathinfo( $metadata[ $style_key ], PATHINFO_FILENAME ) . '.ts';
+
+			if ( ! file_exists( $auto_import_file ) ) {
+				file_put_contents(
+					$auto_import_file,
+					sprintf(
+						"import './%s';\n",
+						remove_block_asset_path_prefix( $metadata[ $style_key ] )
+					)
+				);
+			}
+
+			return 'file:./' . basename( $auto_import_file );
 		}
 
-		return $metadata;
-	}
-
-	public function get_block_asset_args( string $block_path ): array {
-		$block_args = array();
-
-
-
-		return $block_args;
+		return false;
 	}
 
 	private function prepare_block_assets( array $metadata ): array {
+		$block_path = dirname( $metadata['file'] );
 		$block_assets = array();
 
-		foreach ( $this->script_fields as $key ) {
-			$style_key = str_replace( 'cript', 'tyle', $key );
+		foreach ( $this->script_fields as $script_key ) {
+			$style_key = str_replace( 'cript', 'tyle', $script_key );
+			$css_only = false;
+			$style_import = $this->maybe_generate_style_import( $metadata, $style_key, $script_key );
 
-			if ( $metadata[ $style_key ] && is_null($metadata[ $key ]) ) {
-				// we have style but no script, so we need to handle it differently
-				// but for now this is not supported, so notify user
-				trigger_error( sprintf( 'Block %s has %s but no corresponding script, this is not supported yet', $metadata['name'], $style_key ), E_USER_WARNING );
-			} elseif ( $metadata[ $key ] ) {
+			if ( $style_import ) {
+				$metadata[ $script_key ] = $style_import;
+				$css_only = true;
+			}
+
+			if ( $metadata[ $script_key ] ) {
 				// ensure we have array, since $metadata[ $key ] can also be a string
-				$script_value = is_array( $metadata[ $key ] ) ? $metadata[ $key ] : [ $metadata[ $key ] ];
+				$script_value = is_array( $metadata[ $script_key ] ) ? $metadata[ $script_key ] : [ $metadata[ $script_key ] ];
 				foreach ( $script_value as $index => $script ) {
 					if ( str_starts_with( $script, 'file:./' ) ) {
-						$block_path = dirname( $metadata['file'] );
 						$script_path = remove_block_asset_path_prefix( $script );
 						$src_path = str_replace( ABSPATH, '', $block_path ) . '/' . $script_path;
-						$handle = generate_block_asset_handle( $metadata['name'], $key, $index );
+						$handle = generate_block_asset_handle( $metadata['name'], $script_key, $index );
 
 						$block_assets[ $script ] = array(
 							'type' => 'script',
-							'place' => $key,
+							'place' => $script_key,
 							'src' => $src_path,
 							'handle' => $handle,
+							'css-only' => $css_only,
 						);
 
 						// if we have corresponding style, it will be registered together
@@ -129,6 +142,44 @@ class Assets extends Utils\Singleton {
 		}
 
 		return $block_assets;
+	}
+
+	/**
+	 * Register assets and replace handles in metadata.
+	 * $metadata is modified in place.
+	 *
+	 * @param array $metadata
+	 * @param array $block_assets
+	 *
+	 * @return array
+	 */
+	private function register_assets_and_replace_handles( array $metadata, array $block_assets ): array {
+		foreach ( $block_assets as $script => $args ) {
+			if ( $args['type'] === 'script' ) {
+				Vite\register_asset(
+					$this->dist_dir,
+					$args['src'],
+					array(
+						'handle' => $args['handle'],
+						'css-only' => $args['css-only'],
+					),
+				);
+			}
+
+			// replace the original script with the registered handle
+			if ( is_array( $metadata[ $args['place'] ] ) ) {
+				$metadata[ $args['place'] ] = array_map(
+					function ($value) use ($args, $script) {
+						return $value === $script ? $args['handle'] : $value;
+					},
+					$metadata[ $args['place'] ]
+				);
+			} else {
+				$metadata[ $args['place'] ] = $args['handle'];
+			}
+		}
+
+		return $metadata;
 	}
 
 	public function update_block_type_metadata( array $metadata ): array {
@@ -156,30 +207,7 @@ class Assets extends Utils\Singleton {
 				update_option( $block_slug . '-block-assets', $option );
 			}
 
-			// Register block assets
-			foreach ( $option['block_assets'] as $script => $args ) {
-				if ( $args['type'] === 'script' ) {
-					Vite\register_asset(
-						$this->dist_dir,
-						$args['src'],
-						array(
-							'handle' => $args['handle'],
-						),
-					);
-				}
-
-				// replace the original script with the registered handle
-				if ( is_array( $metadata[ $args['place'] ] ) ) {
-					$metadata[ $args['place'] ] = array_map(
-						function( $value ) use ( $args, $script ) {
-							return $value === $script ? $args['handle'] : $value;
-						},
-						$metadata[ $args['place'] ]
-					);
-				} else {
-					$metadata[ $args['place'] ] = $args['handle'];
-				}
-			}
+			$metadata = $this->register_assets_and_replace_handles( $metadata, $option['block_assets'] );
 		}
 
 		return $metadata;
