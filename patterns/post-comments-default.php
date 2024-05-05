13c44f7c56f525a1c247dcbd047fb92ad51626a9
Urszula Wielgosz <urszula@wielgosz.info>
2024-05-05 17:24:31 +0000

    Comments styling

--- patterns/post-comments-default.php
+++ patterns/post-comments-default.php
@@ -13,34 +13,38 @@
 <!-- wp:group {"layout":{"type":"constrained"},"metadata":{"name":"Comments"}} -->
 <div class="wp-block-group">
 	<!-- wp:comments -->
-	<div class="wp-block-comments"><!-- wp:comments-title /-->
+	<div class="wp-block-comments">
+		<!-- wp:comments-title {"showPostTitle":false,"showCommentsCount":false} /-->
 
-		<!-- wp:comment-template -->
-		<!-- wp:columns -->
-		<div class="wp-block-columns"><!-- wp:column {"width":"40px"} -->
-			<div class="wp-block-column" style="flex-basis:40px">
-				<!-- wp:avatar {"size":40,"style":{"border":{"radius":"20px"}}} /-->
-			</div>
-			<!-- /wp:column -->
+		<!-- wp:comment-template {"style":{"spacing":{"padding":{"left":"var:preset|spacing|50"}}}} -->
+		<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}},"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"var:preset|spacing|50","bottom":"0","left":"0","right":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base-2","textColor":"contrast-2","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+		<div class="wp-block-group has-contrast-2-color has-base-2-background-color has-text-color has-background has-link-color"
+			style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:0">
+			<!-- wp:avatar {"size":130,"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} /-->
 
-			<!-- wp:column -->
-			<div class="wp-block-column"><!-- wp:comment-author-name {"fontSize":"small"} /-->
+			<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|40"}}},"className":"wi-comment-inner","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group wi-comment-inner" style="padding-top:var(--wp--preset--spacing--40)">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"className":"wi-comment-meta","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+				<div class="wp-block-group wi-comment-meta">
+					<!-- wp:comment-author-name {"isLink":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"contrast","fontSize":"large"} /-->
 
-				<!-- wp:group {"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}},"layout":{"type":"flex"}} -->
-				<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
-					<!-- wp:comment-date {"fontSize":"small"} /-->
-
-					<!-- wp:comment-edit-link {"fontSize":"small"} /-->
+					<!-- wp:comment-date {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"small"} /-->
 				</div>
 				<!-- /wp:group -->
 
-				<!-- wp:comment-content /-->
+				<!-- wp:comment-content {"fontSize":"small"} /-->
 
-				<!-- wp:comment-reply-link {"fontSize":"small"} /-->
+				<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+				<div class="wp-block-group">
+					<!-- wp:comment-edit-link {"style":{"typography":{"textTransform":"uppercase"}},"backgroundColor":"accent","fontSize":"small"} /-->
+
+					<!-- wp:comment-reply-link {"style":{"typography":{"textTransform":"uppercase"}},"backgroundColor":"accent","fontSize":"small"} /-->
+				</div>
+				<!-- /wp:group -->
 			</div>
-			<!-- /wp:column -->
+			<!-- /wp:group -->
 		</div>
-		<!-- /wp:columns -->
+		<!-- /wp:group -->
 		<!-- /wp:comment-template -->
 
 		<!-- wp:comments-pagination {"layout":{"type":"flex","justifyContent":"center"}} -->
