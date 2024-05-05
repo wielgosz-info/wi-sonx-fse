dff65a24330ef96fc0e0994ddfaecb0a13feda4a
Urszula Wielgosz <urszula@wielgosz.info>
2024-05-05 17:34:38 +0000

    responsive updates

--- patterns/post-comments-default.php
+++ patterns/post-comments-default.php
@@ -17,14 +17,14 @@
 		<!-- wp:comments-title {"showPostTitle":false,"showCommentsCount":false} /-->
 
 		<!-- wp:comment-template {"style":{"spacing":{"padding":{"left":"var:preset|spacing|50"}}}} -->
-		<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}},"spacing":{"blockGap":"var:preset|spacing|60","padding":{"top":"var:preset|spacing|50","bottom":"0","left":"0","right":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base-2","textColor":"contrast-2","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
-		<div class="wp-block-group has-contrast-2-color has-base-2-background-color has-text-color has-background has-link-color"
+		<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"0","left":"0","right":"var:preset|spacing|50"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"base-2","textColor":"contrast-2","className":"wi-comment","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+		<div class="wp-block-group wi-comment has-contrast-2-color has-base-2-background-color has-text-color has-background has-link-color"
 			style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:0">
 			<!-- wp:avatar {"size":130,"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} /-->
 
 			<!-- wp:group {"style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|40"}}},"className":"wi-comment-inner","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","flexWrap":"nowrap"}} -->
 			<div class="wp-block-group wi-comment-inner" style="padding-top:var(--wp--preset--spacing--40)">
-				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"className":"wi-comment-meta","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"bottom"}} -->
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"className":"wi-comment-meta","layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"bottom"}} -->
 				<div class="wp-block-group wi-comment-meta">
 					<!-- wp:comment-author-name {"isLink":false,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"contrast","fontSize":"large"} /-->
 
