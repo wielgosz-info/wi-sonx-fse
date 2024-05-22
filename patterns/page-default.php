d325a63d002bc08cd0cd634769e39a258a5fc9b9
Urszula Wielgosz <urszula@wielgosz.info>
2024-05-22 07:55:53 +0000

    Basic page template. Allows page comments as per requirements

--- patterns/page-default.php
+++ patterns/page-default.php
@@ -12,15 +12,21 @@
 
 <!-- wp:group {"tagName":"article","align":"wide","layout":{"type":"constrained"},"metadata":{"name":"Article"}} -->
 <article class="wp-block-group alignwide">
-	<!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-		<!-- wp:post-title {"level":1} /-->
+	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
+	<div class="wp-block-group alignwide"
+		style="margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60)">
+		<!-- wp:post-title {"level":1,"fontSize":"xx-large"} /-->
+
+		<!-- wp:x3p0/breadcrumbs {"separator":"slash","separatorType":"text","style":{"elements":{"link":{"color":{"text":"var:preset|color|off-contrast"},":hover":{"color":{"text":"var:preset|color|contrast"}}}}},"textColor":"off-contrast","fontSize":"small"} /-->
 	</div>
 	<!-- /wp:group -->
 
-	<!-- wp:group {"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group">
-		<!-- wp:post-content /-->
+	<!-- wp:post-featured-image {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} /-->
+
+	<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group"
+		style="margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--60)">
+		<!-- wp:post-content {"style":{"spacing":{"blockGap":"var:preset|spacing|50"},"typography":{"lineHeight":"1.8"}},"textColor":"contrast-2"} /-->
 	</div>
 	<!-- /wp:group -->
 </article>
