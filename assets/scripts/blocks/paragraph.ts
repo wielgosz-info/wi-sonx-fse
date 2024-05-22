211f3bae2b71c4a29b250d2c8678124171bc68a4
Urszula Wielgosz <urszula@wielgosz.info>
2024-05-22 14:23:56 +0000

    Support existing paragraph styles & lang attr

---
+++ assets/scripts/blocks/paragraph.ts
@@ -0,0 +1,11 @@
+/* global wp */
+
+wp.blocks.registerBlockStyle('core/paragraph', {
+	name: 'paragraph-note',
+	label: 'Note',
+});
+
+wp.blocks.registerBlockStyle('core/paragraph', {
+	name: 'paragraph-series',
+	label: 'Series',
+});
